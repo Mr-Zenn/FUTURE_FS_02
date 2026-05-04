de81768542648a2efab1fe59f83152428175434e
Mr-Zenn <jayprakash6231ssb@gmail.com>
2026-05-04 11:05:56 +0530

    fix: remove localhost proxy for production

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "https://mini-crm-server-x8v1.onrender.com",
-    },
-  },
+  plugins: [react()]
 })
